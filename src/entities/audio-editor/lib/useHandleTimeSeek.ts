55638e764f63e05e900cff61495db35785572015
Maga Lutfullaev <98145444+mlutfullaev@users.noreply.github.com>
2024-09-26 19:18:33 +0500

    Fix: timeline scroll x on ruler mouse move (#4)

--- src/entities/audio-editor/lib/useHandleTimeSeek.ts
+++ src/entities/audio-editor/lib/useHandleTimeSeek.ts
@@ -1,11 +1,13 @@
 import { useCallback } from 'react';
 
-import { Player, Timeline } from '../model';
+import { Player, shiftXTimeline, Timeline } from '../model';
 
 export const useHandleTimeSeek = (player: Player, timeline: Timeline) => {
   return useCallback(
-    (e: MouseEvent | React.MouseEvent<HTMLDivElement>) =>
-      player.setTime(timeline.globalToTime(e.pageX)),
+    (e: MouseEvent | React.MouseEvent<HTMLDivElement>) => {
+      player.setTime(timeline.globalToTime(e.pageX));
+      shiftXTimeline(e.pageX, timeline);
+    },
     [player, timeline],
   );
 };
